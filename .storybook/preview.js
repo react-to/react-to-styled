570140072badfe751f5eb0f48e7c090c8cabd58d
aleksaToljic <aleksa.toljic@gmail.com>
2022-01-31 23:59:12 +0100

    build(stories): First show Overview stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,11 @@
 export const parameters = {
   options: {
-    storySort: {
-      order: ['Introduction', '*'],
+    storySort: ([, { kind: a }], [, { kind: b }]) => {
+      if (b.includes('Overview')) {
+        return +1
+      }
+
+      return (a > b) - (a < b)
     },
   },
   controls: { expanded: true, hideNoControlsWarning: true },
