a7148bb77e18b6bcc904c23a38e411cfaee8b63a
aleksaToljic <aleksa.toljic@gmail.com>
2022-02-05 14:50:49 +0100

    chore(root): Update changelog config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: { 'subject-case': [2, 'always', 'sentence-case'] },
+  rules: {
+    'subject-case': [2, 'always', 'sentence-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'types',
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 }
