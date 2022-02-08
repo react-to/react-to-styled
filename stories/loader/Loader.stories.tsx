83d5432c7cc609eb029e32209ed9c481cffed7ee
toljaEmbroker <aleksa.toljic@embroker.com>
2022-02-08 10:51:27 +0100

    feat(root): Add Loader stories

---
+++ stories/loader/Loader.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta } from '@storybook/react'
+import React from 'react'
+import { Loader } from '@react-to-styled/loader'
+
+export default {
+  title: 'Loader/All stories',
+  component: Loader,
+  parameters: {
+    controls: {
+      exclude: /.*/s,
+    },
+  },
+} as Meta
+
+export const LoaderWrapper = () => {
+  return <Loader wrapperHeight="100%" />
+}
+
+LoaderWrapper.storyName = 'Loader 100% wrapper'
+
+export const LoaderFixedWrapper = () => {
+  return <Loader wrapperHeight={300} />
+}
+
+LoaderFixedWrapper.storyName = 'Loader 300px wrapper'
+
+export const LoaderCustomSizes = () => {
+  const sizes = [16, 32, 48, 72, 128]
+
+  return sizes.map(size => <Loader key={size} loaderSize={size} />)
+}
+
+LoaderCustomSizes.storyName = 'Custom size loaders'
---
+++ stories/loader/Properties.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import { Loader, LoaderProps } from '@react-to-styled/loader'
+
+export default {
+  title: 'Loader/Properties',
+  component: Loader,
+  args: {
+    wrapperHeight: 300,
+    loaderSize: 100,
+  },
+  argTypes: {
+    wrapperHeight: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 1000,
+        step: 100,
+      },
+    },
+    loaderSize: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 150,
+        step: 10,
+      },
+    },
+  },
+} as Meta<LoaderProps>
+
+export const LoaderProperties: Story<LoaderProps> = args => {
+  return <Loader {...args} />
+}
+
+LoaderProperties.storyName = 'Properties'
